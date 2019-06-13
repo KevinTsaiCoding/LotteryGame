989e5fe68324ce89b0a145fb768be9bef6c1e09b
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-06-13 23:31:35 +0800

    Update 3_stars.c

--- source(.c)/3_stars.c
+++ source(.c)/3_stars.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
 #include<windows.h>
+#include<string.h>
+#include"ref.h"
 #define N 4
 void best(void);/* 正彩 */
 void worse(void);/* 組彩 */
 void worst(void);/* 對彩 */
 char stars_num[N];/* 正確號碼 */
 char player_num[N];	/* 玩家號碼 */
-int i,j,a=0,b=0,choice,random;
+int i,j,a=0,b=0,random;
+char choice;
 void stars(void)
 {
+	
 	system("cls");
-	for(i=0;i<N-1;i++){
-		stars_num[i]=rand()%10+48; // generate ASCII code 0~9
-		for(j=0;j<i;j++){
-			if(stars_num[i]==stars_num[j]){
-				i--;
-			}
-		}
-	}
 	printf("\t■■■■■\n\t        ■\n\t        ■\n\t■■■■■\n\t        ■\n\t        ■\n\t■■■■■\n\n");
 	sleep(1);
 	system("cls");
@@ -36,52 +33,106 @@ void stars(void)
 	system("cls");
 	printf("\t■■■■■\n\t■     \n\t■     \n\t■■■■■\n\t        ■\n\t        ■\n\t■■■■■\n");
 	sleep(1);
+	for(i=0;i<N-1;i++){
+		stars_num[i]=rand()%10+48; // generate ASCII code 0~9
+	}
+	again:
+	st:
+	system("Mode con cols=40 lines=17");
+	SetConsoleTitle("3 Stars");
+	system("color 16");
 	system("cls");
 	puts("歡迎游完三星彩, 請選擇您要玩的模式");
 	puts("1. 正彩");
 	puts("2. 組彩");
 	puts("3. 對彩");
+	puts("4. 返回主目錄");
+	puts("5. 玩法說明");
 	printf("> ");
-	int op;
-	scanf("%d",&op);
+	char op;
+	scanf("%c",&op);
+	fflush(stdin);
 	switch(op)
 	{
-		case 1:
+		case '1':
 			best();
 			break;
-		case 2:
+		case '2':
 			worse();
 			break;
-		case 3:
+		case '3':
 			worst();
 			break;
+		case '4':
+			goto content;
+			break;
+		case '5':
+			ref();
+			goto st;
+			break;
 		default:
-			puts("No Support");
+			system("cls");
+			SetConsoleTitle("Error 404");
+			system("color 02");
+			printf("\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\t        ■\n\t        ■\n\t        ■\n\t        ■\n\n");
+			sleep(1);
+			system("cls");
+			printf("\t■■■■■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\n\n");
+			sleep(1);
+			system("cls");
+			printf("\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\t        ■\n\t        ■\n\t        ■\n\t        ■\n\n");
+			sleep(1);
+			system("cls");
+			goto again;
 			break;
 	}
+	content:
+	system("cls");
+	system("color 07");
+	printf("返回目錄中...\n");
+	Sleep(700);
 }
 void best(void)
 {
+	again:
 	SetConsoleTitle("S Stars- 正彩");
 	fflush(stdin);
 	puts("選個三位數: ");
 	printf("1.電腦選號\n2.玩家選號\n> ");
-	scanf("%d",&choice);
-	if(choice==2){
+	scanf(" %c",&choice);
+	fflush(stdin);
+	if(choice=='2'){
 		fflush(stdin);
 		printf("玩家選擇號碼為\n> ");
 		gets(player_num);
 	}
-	else if(choice==1){
+	else if(choice=='1'){
 		 printf("電腦選擇號碼為\n> ");
 		for(random=0;random<N-1;random++){
 			player_num[random]=rand()%10+48;
 			printf("%c",player_num[random]);
 		}	
 	}
+	else{
+		system("cls");
+		SetConsoleTitle("Error 404");
+		system("color 02");
+		printf("\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\t        ■\n\t        ■\n\t        ■\n\t        ■\n\n");
+		sleep(1);
+		system("cls");
+		printf("\t■■■■■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\n\n");
+		sleep(1);
+		system("cls");
+		printf("\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\t        ■\n\t        ■\n\t        ■\n\t        ■\n\n");
+		sleep(1);
+		system("cls");
+		goto again;
+	}
+		
 	puts("");
 	int bet;
-	printf("您下注數目(每注25元): ");
+	printf("您下注數目(每注25元)\n");
+	printf("> ");
 	scanf("%d",&bet);	
 	int i,j;
 	int a=0,b=0;
@@ -101,7 +152,7 @@ void best(void)
 	system("cls");
 	for(i=0;i<N-1;i++){
 		if(stars_num[i]=='0')
-			printf("\t■■■■■\n\t■      ■\n■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\n\n");
+			printf("\t■■■■■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■      ■\n\t■■■■■\n\n\n");
 		if(stars_num[i]=='1')
 			printf("\t■\n\t■\n\t■\n\t■\n\t■\n\t■\n\t■\n\n");
 		if(stars_num[i]=='2')
@@ -124,7 +175,8 @@ void best(void)
 		system("cls");
 	}
 	SetConsoleTitle("Result");
-	(a==3)?printf("恭喜獲得正彩, 您獲得%d元\n",bet*25*500):printf("您獲得0元\n",bet);	
+	(a==3)?printf("恭喜獲得正彩, 您獲得%d元\n",bet*25*500):printf("您獲得0元\n",bet);
+	system("pause");
 }
 
 void worse(void)
@@ -148,7 +200,8 @@ void worse(void)
 	}
 	puts("");
 	int bet;
-	printf("您下注數目(每注25元): ");
+	printf("您下注數目(每注25元)\n");
+	printf("> ");
 	scanf("%d",&bet);
 	a=0,b=0;
 	for(i=0;i<N-1;i++){
@@ -159,6 +212,12 @@ void worse(void)
 				if(stars_num[i]==player_num[j])
 					b++;
 		}
+	}
+	int same;
+	same=0;
+	for(i=0;i<N-1;i++){
+		if(stars_num[i]==stars_num[i+1])
+			same++;
 	}
 	SetConsoleTitle("S Stars- 組彩 ~ 開獎中...");
 	system("cls");
@@ -190,7 +249,13 @@ void worse(void)
 		system("cls");
 	}
 	SetConsoleTitle("Result");
-	(a==3||b==3)?printf("恭喜獲得組彩, 您獲得%d元\n",bet*25*80):printf("恭喜獲得正彩, 您獲得%d元\n",0);
+	if((a==3||b==3)&&same==2)
+		printf("恭喜獲得3組彩, 您獲得%d元\n",bet*25*160);
+	else if((a==3||b==3)&&same==0)
+		printf("恭喜獲得6組彩, 您獲得%d元\n",bet*25*80);
+	else
+		printf("您獲得%d元\n",0);
+	system("pause");
 }
 void worst(void)
 {
@@ -213,7 +278,8 @@ void worst(void)
 	}
 	puts("");
 	int bet;
-	printf("您下注數目(每注25元): ");
+	printf("您下注數目(每注25元)\n");
+	printf("> ");
 	scanf("%d",&bet);
 	for(i=0;i<2;i++)
 		if(stars_num[i]==player_num[i])
@@ -251,5 +317,6 @@ void worst(void)
 		system("cls");
 	}
 	SetConsoleTitle("Result");
-	(a==2||b==2)?printf("恭喜獲得對彩, 您獲得%d元\n",bet*25*30):printf("您獲得0元\n",bet);	
+	(a==2||b==2)?printf("恭喜獲得對彩, 您獲得%d元\n",bet*25*30):printf("您獲得0元\n",bet);
+	system("pause");
 }
